7185b6b977af4932c0150d34c905f923b7bf2cbc
nikgub_ <danger8217.nik@gmail.com>
2024-05-08 02:01:16 +0300

    Added AccursedContractItem blob

---
+++ src/main/java/xyz/nikgub/zweihander/common/items/AccursedContractItem.java
@@ -0,0 +1,9 @@
+package xyz.nikgub.zweihander.common.items;
+
+import net.minecraft.world.item.Item;
+
+public class AccursedContractItem extends Item {
+    public AccursedContractItem(Properties properties) {
+        super(properties);
+    }
+}
